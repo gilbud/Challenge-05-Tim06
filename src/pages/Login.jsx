7c3a03e5162ebc0ca7235dac3a6d6214f8f6b7c6
Gilang Budiarto <87963358+gilbud@users.noreply.github.com>
2023-05-05 02:04:11 +0700

    feature: login page (#1)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,70 @@
-import React from "react";
+import { React, useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
 import NavbarNoSearch from "../components/NavbarNoSearch";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let data = JSON.stringify({
+        email,
+        password,
+      });
+
+      let config = {
+        method: "post",
+        url: `https://km4-challenge-5-api.up.railway.app/api/v1/auth/login`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      const response = await axios.request(config);
+      const { token } = response.data.data;
+
+      localStorage.setItem("token", token);
+
+      // navigate("/");
+
+      // Temporary solution
+      window.location.href = "/";
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <>
       <NavbarNoSearch />
       <Container className="p-5 mt-5">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="text-light">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className="text-light">Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
         </Form.Group>
-        <Button variant="warning" type="submit">
+        <Button variant="warning" type="submit" onClick={onSubmit}>
           Login
         </Button>
       </Container>
